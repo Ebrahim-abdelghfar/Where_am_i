c2fcc30b29a29c31948c9d8098c4387440896daa
Ebrahim-abdelghfar <ebrahimabdelghfar550@gmail.com>
2021-08-21 16:12:58 +0200

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ebrahim/where/src
+# Build directory: /home/ebrahim/where/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(teleop_twist_keyboard)
+subdirs(my_robot)
---
+++ build/pgm_map_creator/msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ebrahim/where/src/pgm_map_creator/msgs
+# Build directory: /home/ebrahim/where/build/pgm_map_creator/msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
